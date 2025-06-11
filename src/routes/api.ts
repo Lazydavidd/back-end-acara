84f0b6676b7644ebe1ede05b1e24ddad8712c33b
Nama Kamu <dafaraihan062@gmail.com>
2025-06-11 18:21:38 +0700

    feat: init project

---
+++ src/routes/api.ts
@@ -0,0 +1,8 @@
+import express, { Router } from "express";
+import dummyController from "../controllers/dummy.controller";
+
+const router = express.Router();
+
+router.get("/dummy", dummyController.dummy);
+
+export default router;
