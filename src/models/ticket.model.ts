6848b4903d3db5ce098453aeae11bbedab9f9719
Lazydavidd <dafaraihan062@gmail.com>
2025-07-07 13:14:08 +0700

    feat: Add CRUD Ticket

---
+++ src/models/ticket.model.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+import * as Yup from "yup";
+import { EVENT_MODEL_NAME } from "./event.model";
+
+export const TICKET_MODEL_NAME= "Ticket";
+
+export const ticketDAO = Yup.object({
+    price: Yup.number().required(),
+    name: Yup.string().required(),
+    events: Yup.string().required(),
+    description: Yup.string().required(),
+    quantity: Yup.number().required(),
+});
+
+export type TypeTicket = Yup.InferType<typeof ticketDAO>;
+
+interface Ticket extends Omit<TypeTicket, "events"> {
+    events: Schema.Types.ObjectId;
+}
+
+const TicketSchema = new Schema<Ticket>({
+    price: { type: Number, required: true },
+    name: { type: String, required: true },
+    events: { type: Schema.Types.ObjectId, ref: EVENT_MODEL_NAME, required: true },
+    description: { type: String, required: true },
+    quantity: { type: Schema.Types.Number, required: true },
+}, 
+{
+    timestamps: true,
+});
+
+const TicketModel = mongoose.model(TICKET_MODEL_NAME, TicketSchema);
+export default TicketModel;
